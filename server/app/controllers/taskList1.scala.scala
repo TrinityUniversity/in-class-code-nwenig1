12f97661a772214bd29e6f40622cac0eb5c25fa4
2022-08-22 <nwenig@pandora06.cs.trinity.edu>
2024-02-26 11:55:12 -0600

    Videos through 12ish

--- server/app/controllers/taskList1.scala.scala
+++ server/app/controllers/taskList1.scala.scala
@@ -10,46 +10,49 @@ import models.TaskListInMemoryModel
 @Singleton 
 class taskList1 @Inject()(cc:ControllerComponents) extends AbstractController(cc) {
 
-    def login = Action{
+    def login = Action{ implicit request=>
         Ok(views.html.login1())
     }
     def validateLoginGet(username: String, password: String) = Action{
         Ok(s"$username logging in with $password")
     }
-    def validateLoginPost = Action { request =>
+    def validateLoginPost = Action { implicit request =>
         val postVals = request.body.asFormUrlEncoded
         postVals.map { args =>
             val username=args("username").head 
             val password=args("password").head 
             if(TaskListInMemoryModel.validateUser(username, password)){
-
-            
-            Redirect(routes.taskList1.taskList)
+            Redirect(routes.taskList1.taskList).withSession("username"-> username)
             }else{
-                Redirect(routes.taskList1.login)
+                Redirect(routes.taskList1.login).flashing("error" -> "Invalid username/password combination.")
             }
             }.getOrElse(Redirect(routes.taskList1.login))
     }
 
-    def taskList= Action{
-        val username = "Mark"
-        val password="Pass"
-        val tasks=TaskListInMemoryModel.getTasks(username)
-        Ok(views.html.taskList1(tasks))
+    def taskList= Action{ implicit request=>
+        val usernameOption = request.session.get("username")
+        usernameOption.map{ username =>
+            val tasks=TaskListInMemoryModel.getTasks(username)
+             Ok(views.html.taskList1(tasks))
+            }.getOrElse(Redirect(routes.taskList1.login))
+        
+       
     }
-    def createUser = Action { request =>
+    def createUser = Action { implicit request =>
         val postVals = request.body.asFormUrlEncoded
         postVals.map { args =>
             val username=args("username").head
             val password=args("password").head 
             if(TaskListInMemoryModel.createUser(username, password)){
-                Redirect(routes.taskList1.taskList)
+                Redirect(routes.taskList1.taskList).withSession("username"-> username)
             } else {
-                Redirect(routes.taskList1.login)
+                Redirect(routes.taskList1.login).flashing("error" -> "User Creation Failed")
             }
             }.getOrElse(Redirect(routes.taskList1.login))
         
     }
-
+    def logout = Action{
+        Redirect(routes.taskList1.login).withNewSession
+    }
   
 }
